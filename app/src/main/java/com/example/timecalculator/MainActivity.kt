9490021c8c36f76c4989af678fb42b5d3e949b2a
maks2001916 <mamonov.maksim.20003@gmail.com>
2024-09-29 13:49:19 +0500

    добавлен toolbar, измеён цвет кнопок, добавлено меню с кнопками очистки полей ввода/вывода и выхода

--- app/src/main/java/com/example/timecalculator/MainActivity.kt
+++ app/src/main/java/com/example/timecalculator/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.timecalculator
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbarMain: Toolbar
+
     private lateinit var firstOperandET: EditText
     private lateinit var secondOperandET: EditText
 
@@ -20,28 +24,49 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var resultTV: TextView
 
-    private lateinit var resetBTN: Button
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = R.string.timeCalculator.toString()
+        toolbarMain.subtitle = R.string.version.toString()
+        toolbarMain.setLogo(R.drawable.ic_calculator)
+
         firstOperandET = findViewById(R.id.firstOperandET)
         secondOperandET = findViewById(R.id.lastOperandET)
         buttonSumBTN = findViewById(R.id.buttonSumBTN)
         buttonDefBTN = findViewById(R.id.buttonDifBTN)
         resultTV = findViewById(R.id.resultTV)
-        resetBTN = findViewById(R.id.resetBTN)
 
         buttonSumBTN.setOnClickListener(this::onClick)
         buttonDefBTN.setOnClickListener(this::onClick)
-        resetBTN.setOnClickListener(this::onClick)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.resetMenuMain -> {
+                firstOperandET.text.clear()
+                secondOperandET.text.clear()
+                resultTV.text = R.string.result.toString()
+            }
+            R.id.exitMenuMain -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun onClick(v: View?) {
@@ -62,11 +87,7 @@ class MainActivity : AppCompatActivity() {
                 mathematicalOperations.transformation(firstOperand),
                 mathematicalOperations.transformation(secondOperand)
             )
-        R.id.resetBTN -> {
-                firstOperandET.text.clear()
-                secondOperandET.text.clear()
-                resultTV.text = ""
-            }
+
         }
         resultTV.text = mathematicalOperations.formatTime(result)
     }
